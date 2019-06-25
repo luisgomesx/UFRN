959f21375c2f66a943d1b590d57dd1d2f0e04ae4
Luís Gomes <45674351+luisgomesx@users.noreply.github.com>
2019-06-25 13:22:10 -0300

    Add files via upload

--- extract.py
+++ extract.py
@@ -35,10 +35,51 @@ from datetime import datetime
 
 temp = [w.replace(':', '.').replace('/', '.') for w in dados_requisicao_data_hora_tratado]
 print(temp)
+print(len(temp))
 
-'''GERANDO ERRO !!!!!!!!!!!'''
-tempo_milisegundos = []
+print(len(temp))
+
+def remove_repetidos(lista):
+    l = ['annot', 'he', 'ine', 'ain.cpp.51.']
+    for i in lista:
+        if i in l:
+            lista.remove(i)
+    return lista
+
+temp2 = remove_repetidos(temp)
+
+print(temp2)
+print(len(temp2))
+
+'''
+tempo em milisegundos de um dia 86400000
+GERANDO ERRO ABAIXO!!!!!!!!!!!
+tempos com ('he', 'ine', 'annot') dentro dos dados
+'''
+
+tempos = list()
 for i in temp:
-    tempo_milisegundos += datetime.strptime(i, '%d.%b.%Y.%H.%M.%S')
+    x = datetime.strptime(i, '%d.%b.%Y.%H.%M.%S')
+    print(x)
+    #tempos.extend(i)
 
-print(tempo_milisegundos)
+#print(tempos)
+    #x = x.timestamp()*1000
+    #tempos.extend(x)
+
+#tempos_milisegundos = tempos.timestamp()*1000
+
+#print(tempos)
+
+
+'''
+test = datetime.strptime('29.Nov.2017.00.00.00', '%d.%b.%Y.%H.%M.%S')
+milisec = test.timestamp()*1000
+print(milisec)
+
+test2 = datetime.strptime('30.Nov.2017.00.00.00', '%d.%b.%Y.%H.%M.%S')
+milisec2 = test2.timestamp()*1000
+print(milisec2)
+
+print(milisec2 - milisec)
+'''
